3e2f70389bba13d1b854c3d3321836deae3c107c
Sujal Gaurson <sujal90.sja@gmail.com>
2026-02-04 17:09:56 +0530

    fixed ci pipeline and also llm restrictation for llm in test are applied

--- Backend-rag/app/core/llm.py
+++ Backend-rag/app/core/llm.py
@@ -1,12 +1,21 @@
 from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
 from app.core.config import GROQ_API_KEY
+from langchain_core.language_models.fake import FakeListLLM
+import os
 
 def get_llm():
+
+    if os.getenv("TESTING") == "true":
+      return FakeListLLM(
+          responses=["LangChain is a framework for building RAG systems."]
+      )
+    
     # return ChatOpenAI(
     #     model="gpt-4o-mini",
     #     temperature=0
     # )
+    
     return ChatGroq(
         model="llama-3.3-70b-versatile",
         api_key=GROQ_API_KEY,
