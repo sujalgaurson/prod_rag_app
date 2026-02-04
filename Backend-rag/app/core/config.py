16f2f1e74113ae4c36e57024e44816391529249d
Sujal Gaurson <sujal90.sja@gmail.com>
2026-02-04 17:21:14 +0530

    fixed config.py file for ci testing implementaion

--- Backend-rag/app/core/config.py
+++ Backend-rag/app/core/config.py
@@ -3,9 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+TESTING = os.getenv("TESTING", "false").lower() == "true"
+
 # OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") 
 GROQ_API_KEY= os.getenv("GROQ_API_KEY")
 FAISS_INDEX_PATH = "data/faiss_index"
 
-if not GROQ_API_KEY:
+if not TESTING and not GROQ_API_KEY:
     raise ValueError("GROQ_API_KEY not found in environment variables")
